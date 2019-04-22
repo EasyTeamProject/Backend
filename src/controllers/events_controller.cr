3fef6c25916a4d81edae657945af7814a0ab48cf
Alexandre Lairan <lairana@free.fr>
2019-04-22 21:12:34 +0200

    Implement user relation to the system

--- src/controllers/events_controller.cr
+++ src/controllers/events_controller.cr
@@ -6,4 +6,36 @@ class EventsController < ApplicationController
       json events.to_json
     end
   end
+
+  def create
+    service_event = Events::Create.new(Times::Parse::ISO8601Date.new)
+    event = service_event.call(event_params[:name], event_params[:date])
+
+    if event.save
+      service_link = Events::Link.new
+      link = service_link.call(context.current_user_id, event.id, true)
+
+      if link.save
+        respond_with do
+          json event.to_json
+        end
+      else
+        event.destroy
+        respond_with do
+          json({ error: "An error" }.to_json)
+        end
+      end
+    else
+      respond_with do
+        json({ error: "An error" }.to_json)
+      end
+    end
+  end
+
+  private def event_params
+    params.validation do
+      required(:name)
+      required(:date)
+    end
+  end
 end
