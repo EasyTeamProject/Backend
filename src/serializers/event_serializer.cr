e4441b10883945df2ca9490bb6e76962e6f553b2
Alexandre Lairan <lairana@free.fr>
2019-07-08 13:11:43 +0200

    Setup member for event

---
+++ src/serializers/event_serializer.cr
@@ -0,0 +1,24 @@
+require "crinder"
+
+class EventSerializer < Crinder::Base(Event)
+  field id! : Int64, as: id
+  field name : String?
+  field date : Time?
+  field kind : Int32?
+  field status : Int32?
+
+  field created_at : String
+  field updated_at : String
+
+  class Membership < Crinder::Base(Event::Membership)
+    field user_id : Int64?
+    field first_name : String?
+    field last_name : String?
+    field is_admin : Bool?
+    field invitation_date : String
+  end
+
+  class WithInvitation < self
+    field :members, with: EventSerializer::Membership, value: ->{ object.members }
+  end
+end
